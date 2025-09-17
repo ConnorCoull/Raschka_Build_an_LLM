d18712acd6dd0f1a1218fb937a1f79f95d3e018f
Connor <2470724c@student.gla.ac.uk>
2025-09-17 12:59:15 +0100

    End of Chapter 2

--- embedding.py
+++ embedding.py
@@ -1,12 +1,33 @@
 import torch
+from tokeniser import create_dataloader_v1
 
-input_ids = torch.tensor([2, 3, 5, 1])
+vocab_size = 50257  # GPT-2 vocabulary size (Everything after '=' is copilot I'm impressed)
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
-vocab_size = 6
-output_dim = 3
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
 
-torch.manual_seed(123)
-embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-print(embedding_layer.weight)
-print(embedding_layer(torch.tensor([3])))
-print(embedding_layer(input_ids))
+max_length = 4
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs: \n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+#-----------------------------------------------
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+#-----------------------------------------------
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+
