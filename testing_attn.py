66b4dd88ea0f4573d18ef1d38ffd1f20cb5cc6a1
Connor <2470724c@student.gla.ac.uk>
2025-09-18 17:17:54 +0100

    Moving on to self-attention

--- testing_attn.py
+++ testing_attn.py
@@ -16,26 +16,12 @@ inputs = torch.tensor(
     ]
 )
 
-# Step 1 - Compute intermediate values omega (attention scores)
-query = inputs[1]
-attn_scores_2 = torch.empty(inputs.shape[0]) # 2 appears after every var bc it's the second word we calc for
-for i, x_i in enumerate(inputs):
-    attn_scores_2[i] = torch.dot(x_i, query)
-print(attn_scores_2)
+attn_scores = torch.empty(6, 6)
+attn_scores = inputs @ inputs.T
+print(attn_scores)
 
-attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
-#print("Weights: S", attn_weights_2_tmp)
-#print("Sum: ", attn_weights_2_tmp.sum())
+attn_weights = torch.softmax(attn_scores, dim=1)
+print(attn_weights)
 
-attn_weights_2_naive = softmax_naive(attn_scores_2)
-#print("Weights: S", attn_weights_2_naive)
-#print("Sum: ", attn_weights_2_naive.sum())
-
-attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
-print("Weights: S", attn_weights_2)
-print("Sum: ", attn_weights_2.sum())
-
-context_vec_2 = torch.zeros(query.shape)
-for i, x_i in enumerate(inputs):
-    context_vec_2 += attn_weights_2[i]*x_i
-print(context_vec_2)
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
