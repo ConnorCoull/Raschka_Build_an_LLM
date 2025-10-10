523bc978ad0e8901c9d294fe0309eace9194aa28
Connor <2470724c@student.gla.ac.uk>
2025-10-10 07:21:25 +0100

    SelfAttention_v2 verification

--- simplified_self_attention.py
+++ simplified_self_attention.py
@@ -1,5 +1,5 @@
 import torch
-from utils.attention_utils import SelfAttention_v1
+from utils.attention_utils import SelfAttention_v2
 
 inputs = torch.tensor(
     [[0.43, 0.15, 0.89],
@@ -17,5 +17,5 @@ d_in = inputs.shape[1]
 d_out = 2
 
 torch.manual_seed(123)
-sa_v1 = SelfAttention_v1(d_in, d_out)
+sa_v1 = SelfAttention_v2(d_in, d_out)
 print(sa_v1(inputs))
