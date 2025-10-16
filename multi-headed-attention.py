5e5327c792d57d46be6f9d07156c27ab733b4fbc
Connor <2470724c@student.gla.ac.uk>
2025-10-16 17:00:34 +0100

    End of code for Chapter 3.6

--- multi-headed-attention.py
+++ multi-headed-attention.py
@@ -1,5 +1,5 @@
 import torch
-from utils.attention_utils import MultiHeadAttentionWrapper
+from utils.attention_utils import MultiHeadAttention
 inputs = torch.tensor(
     [[0.43, 0.15, 0.89],
      [0.55, 0.87, 0.66],
@@ -12,12 +12,10 @@ inputs = torch.tensor(
 
 torch.manual_seed(123)
 batch = torch.stack((inputs, inputs), dim=0)
-d_in, d_out = 3, 1
-context_length = batch.shape[1]
 
-mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 context_vecs = mha(batch)
-
 print(context_vecs)
-
 print("context_vecs.shape:", context_vecs.shape)
