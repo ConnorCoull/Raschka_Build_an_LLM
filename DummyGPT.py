ed6106da9d1fcfe6f263b016985036760a8d5dea
Connor <2470724c@student.gla.ac.uk>
2025-10-21 17:16:13 +0100

    oo

--- DummyGPT.py
+++ DummyGPT.py
@@ -7,7 +7,7 @@ txt1 = "Every effort moves you"
 txt2 = "Every day holds a"
 
 batch.append(torch.tensor(tokenizer.encode(txt1)))
-batch.aooend(torch.tensor(tokenizer.encode(txt2)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
 batch = torch.stack(batch, dim=0)
 print(batch)
 
