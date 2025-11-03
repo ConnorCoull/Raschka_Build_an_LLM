b6ec6dfd061ea0476b858595212feccf2ce3c229
Connor <2470724c@student.gla.ac.uk>
2025-11-03 07:00:22 +0000

    Moved mixed dropout into main configs

--- utils/gpt2_config.py
+++ utils/gpt2_config.py
@@ -4,7 +4,9 @@ GPT_CONFIG_124M = {
     "emb_dim": 768,
     "n_heads": 12,
     "n_layers": 12,
-    "drop_rate": 0.1,
+    "drop_rate_emb": 0.1,
+    "drop_rate_attn": 0.1,
+    "drop_rate_shortcut": 0.1,
     "qkv_bias": False
 }
 
@@ -14,7 +16,9 @@ GPT_2_MEDIUM = {
     "emb_dim": 1024,
     "n_heads": 16,
     "n_layers": 24,
-    "drop_rate": 0.1,
+    "drop_rate_emb": 0.1,
+    "drop_rate_attn": 0.1,
+    "drop_rate_shortcut": 0.1,
     "qkv_bias": False    
 }
 
@@ -24,7 +28,9 @@ GPT_2_LARGE = {
     "emb_dim": 1280,
     "n_heads": 20,
     "n_layers": 36,
-    "drop_rate": 0.1,
+    "drop_rate_emb": 0.1,
+    "drop_rate_attn": 0.1,
+    "drop_rate_shortcut": 0.1,
     "qkv_bias": False    
 }
 
@@ -34,18 +40,8 @@ GPT_2_XL = {
     "emb_dim": 1600,
     "n_heads": 25,
     "n_layers": 48,
-    "drop_rate": 0.1,
-    "qkv_bias": False    
-}
-
-GPT_CONFIG_124M_mixed_dropout = {
-    "vocab_size": 50257,
-    "context_length": 1024,
-    "emb_dim": 768,
-    "n_heads": 12,
-    "n_layers": 12,
     "drop_rate_emb": 0.1,
     "drop_rate_attn": 0.1,
     "drop_rate_shortcut": 0.1,
-    "qkv_bias": False
+    "qkv_bias": False    
 }
